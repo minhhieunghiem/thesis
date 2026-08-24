71e025d06706483144d55c4635c2805d0c0742d1
minhhieunghiem <hieunghiem2004@gmail.com>
2026-08-24 12:51:30 +0200

    RFC 4493 (CMAC) Test vectors

--- tests/correctness.rs
+++ tests/correctness.rs
@@ -6,6 +6,9 @@ use bckdf::expand::{
 };
 use sha2::Sha256;
 use sha1::Sha1;
+use cmac::{Cmac, Mac};
+use aes::Aes128;
+use digest::KeyInit;
 
 #[test]
 fn cmac_is_deterministic() {
@@ -369,4 +372,59 @@ fn rfc5869_a7_sha1() {
     let expander = HkdfExpand::<Sha1>::default();
     let okm = expander.expand(&prk, &info, 42);
     assert_eq!(okm, expected_okm, "RFC 5869 A.7 OKM mismatch");
+}
+
+#[test]
+fn rfc4493_example1_empty_message() {
+    let key = h("2b7e151628aed2a6abf7158809cf4f3c");
+    let expected = h("bb1d6929e95937287fa37d129b756746");
+
+    let mut mac = Cmac::<Aes128>::new_from_slice(&key).unwrap();
+    let tag = mac.finalize().into_bytes();
+
+    assert_eq!(&*tag, &expected, "RFC 4493 Example 1 (len=0) mismatch");
+}
+
+#[test]
+fn rfc4493_example2_16_bytes() {
+    let key = h("2b7e151628aed2a6abf7158809cf4f3c");
+    let msg = h("6bc1bee22e409f96e93d7e117393172a");
+    let expected = h("070a16b46b4d4144f79bdd9dd04a287c");
+
+    let mut mac = Cmac::<Aes128>::new_from_slice(&key).unwrap();
+    mac.update(&msg);
+    let tag = mac.finalize().into_bytes();
+
+    assert_eq!(&*tag, &expected, "RFC 4493 Example 2 (len=16) mismatch");
+}
+
+#[test]
+fn rfc4493_example3_40_bytes() {
+    let key = h("2b7e151628aed2a6abf7158809cf4f3c");
+    let msg = h("6bc1bee22e409f96e93d7e117393172a\
+                     ae2d8a571e03ac9c9eb76fac45af8e51\
+                     30c81c46a35ce411");
+    let expected = h("dfa66747de9ae63030ca32611497c827");
+
+    let mut mac = Cmac::<Aes128>::new_from_slice(&key).unwrap();
+    mac.update(&msg);
+    let tag = mac.finalize().into_bytes();
+
+    assert_eq!(&*tag, &expected, "RFC 4493 Example 3 (len=40) mismatch");
+}
+
+#[test]
+fn rfc4493_example4_64_bytes() {
+    let key = h("2b7e151628aed2a6abf7158809cf4f3c");
+    let msg = h("6bc1bee22e409f96e93d7e117393172a\
+                     ae2d8a571e03ac9c9eb76fac45af8e51\
+                     30c81c46a35ce411e5fbc1191a0a52ef\
+                     f69f2445df4f9b17ad2b417be66c3710");
+    let expected = h("51f0bebf7e3b9d92fc49741779363cfe");
+
+    let mut mac = Cmac::<Aes128>::new_from_slice(&key).unwrap();
+    mac.update(&msg);
+    let tag = mac.finalize().into_bytes();
+
+    assert_eq!(&*tag, &expected, "RFC 4493 Example 4 (len=64) mismatch");
 }
